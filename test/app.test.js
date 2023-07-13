b3f5fd755937c0826375399d6f0bf6c40da99c02
dylcorman <dylancorman8@gmail.com>
2023-07-12 23:08:33 -0400

    test: added tests for theme routes

--- test/app.test.js
+++ test/app.test.js
@@ -10,7 +10,7 @@ const { User } = require('../models');
 const { faker } = require('@faker-js/faker');
 const { createRandomUser } = require('../utils');
 
-// test home route
+/* // test home route
 describe('GET /', () => {
     it('returns a 200 response', (done) => {
         request(app).get('/')
@@ -183,7 +183,164 @@ describe('DELETE /users/:id', () => {
             throw error;
         })
     })
+}) */
+
+// test themes
+describe('GET /themes', () => {
+    it('returns a 200 response', (done) => {
+        request(app).get('/themes')
+        .expect(200, done);
+    });
+    it('returns a theme with color', (done) => {
+        request(app).get('/themes')
+        .then(result => {
+            console.log('result', result._body.themes[0]);
+            expect(result._body.themes[0]).to.have.property('color');
+            done();
+        })
+    });
+    it('should have more than 1 theme', (done) => {
+        request(app).get('/themes')
+        .then(result => {
+            console.log('result', result._body.themes);
+            expect(result._body.themes.length).to.be.above(1);
+            done();
+        })
+    });
+});
+
+// test POST route
+describe('POST /themes/new', () => {
+    it('should create a new theme and have valid color', (done) => {
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        request(app).post('/themes/new')
+        .type('form')
+        .send({
+            color: randomColor,
+            height: "200px",
+            width: "200px",
+            textSize: "16px"
+        })
+        .then(response => {
+            console.log('new theme created', response._body);
+            expect(response._body.theme.color).to.be.equal(randomColor);
+            done();
+        })
+        .catch(error => {
+            console.log('error', error);
+            throw error;
+        })
+    })
+
+    it('returns a 200 response', (done) => {
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        request(app).post('/themes/new')
+        .type('form')
+        .send({
+            color: randomColor,
+            height: "200px",
+            width: "200px",
+            textSize: "16px"
+        })
+        .expect(200, done);
+    })
+});
+
+// PUT /themes/:id
+describe('PUT /themes/:id', () => {
+    it('should update an existing theme color', (done) => {
+        // create a new theme
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        request(app).post('/themes/new')
+        .type('form')
+        .send({
+            color: randomColor,
+            height: "200px",
+            width: "200px",
+            textSize: "16px"
+        })
+        .then(response => {
+            console.log('new theme created', response._body);
+            const themeId = response._body.theme._id;
+            console.log('--- themeId ---', themeId);
+            const newColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+            // Find the new theme and update
+            request(app).put(`/themes/${themeId}`)
+            .type('form')
+            .send({
+                color: newColor,
+            })
+            .then(updatedResponse => {
+                expect(updatedResponse._body.theme.color).to.be.equal(newColor);
+                done();
+            })
+            .catch(error => {
+                console.log('error', error);
+                throw error;
+            })
+        })
+        .catch(error => {
+            console.log('error', error);
+            throw error;
+        })
+    })
+
+    it('returns a 200 response', (done) => {
+        // create a new theme
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        request(app).post('/themes/new')
+        .type('form')
+        .send({
+            color: randomColor,
+            height: "200px",
+            width: "200px",
+            textSize: "16px"
+        })
+        .then(response => {
+            console.log('new theme created', response._body);
+            const themeId = response._body.theme._id;
+
+            console.log('--- themeId ---', themeId);
+            const newColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+            // Find the new theme and update
+            request(app).put(`/themes/${themeId}`)
+            .type('form')
+            .send({
+                color: newColor,
+            })
+            .expect(200, done);
+        })
+        .catch(error => {
+            console.log('error', error);
+            throw error;
+        })
+    })
 })
 
-// Deliverable 
-// Completing test for all models (GET, POST, PUT, and DELETE)
+// DELETE
+describe('DELETE /themes/:id', () => {
+    it('returns a 200 response', (done) => {
+        // create a new theme
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        request(app).post('/themes/new')
+        .type('form')
+        .send({
+            color: randomColor,
+            height: "200px",
+            width: "200px",
+            textSize: "16px"
+        })
+        .then(response => {
+            const themeId = response._body.theme._id;
+            // Find the new theme and remove
+            request(app).delete(`/themes/${themeId}`)
+            .expect(200, done);
+        })
+        .catch(error => {
+            console.log('error', error);
+            throw error;
+        })
+    })
+})
+
+
