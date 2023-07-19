ae2df6a5566ef754f2a4fbc550d5ac67ee79f22b
LeifCastle <leifurcastle@gmail.com>
2023-07-18 21:34:00 -0700

    feat: removed unneccessary code from template and user model

--- models/user.js
+++ models/user.js
@@ -1,24 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // create the user schema
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: String,
     lastName: String,
     email: { type: String, required: true, unique: true },
-    jobTitle: String,
-    birthdate: Date,
     password: { type: String, required: true },
-    address: {
-        streetAddress: String,
-        city: String,
-        state: String,
-        zipCode: Number
-    },
-    number: String
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 // create model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // export the model to be used
 module.exports = User;
