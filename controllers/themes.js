d27fb3ac60883938809fd1104686d055efe98088
LeifCastle <leifurcastle@gmail.com>
2023-07-22 18:05:48 -0700

    feat: theme model updated and post to themes/new working

--- controllers/themes.js
+++ controllers/themes.js
@@ -1,89 +1,102 @@
 // Imports
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // import the Theme model
-const { Theme } = require('../models');
+const { Theme } = require("../models");
 
 // GET make a themes route to get all themes
-router.get('/', (req, res) => {
-    Theme.find({})
-        .then((themes) => {
-            console.log('themes', themes);
-            res.header("Access-Control-Allow-Origin", "*");
-            res.json({ themes: themes });
-        })
-        .catch((error) => {
-            console.log('error', error);
-            res.header("Access-Control-Allow-Origin", "*");
-            res.json({ message: 'There was an issue, please try again...' });
-        });
+router.get("/", (req, res) => {
+  Theme.find({})
+    .then((themes) => {
+      console.log("themes", themes);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.json({ themes: themes });
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.json({ message: "There was an issue, please try again..." });
+    });
 });
 
 // POST route /themes/new - create a new theme
-router.post('/new', (req, res) => {
-    // read the req.body - data for the new theme coming in at
-    console.log('data from request (theme)', req.body); // object
+router.post("/new", (req, res) => {
+  // read the req.body - data for the new theme coming in at
+  console.log("data from request (theme)", req.body); // object
 
-    // create a theme
-    Theme.create({
-        color: req.body.color,
-        height: req.body.height,
-        width: req.body.width,
-        textSize: req.body.textSize
-    })
-        .then((newTheme) => {
-            console.log('new theme created ->', newTheme);
+  // create a theme
+  Theme.create({
+    name: req.body.name,
+    backgroundColor: req.body.backgroundColor,
+  })
+    .then((theme) => {
+      console.log("new theme created ->", theme);
+      req.body.elements.forEach((element) => {
+        theme.elements.push(element); //Pushes an object with data matching element schema
+      });
+      theme.save().then((newTheme) => {
+        newTheme
+          .populate("elements")
+          .then((result) => {
+            console.log(`Result: ${result}`);
             res.header("Access-Control-Allow-Origin", "*");
             return res.json({ theme: newTheme });
-        })
-        .catch((error) => {
-            console.log('error', error);
-            res.header("Access-Control-Allow-Origin", "*");
-            return res.json({ message: 'error occured, please try again.' });
-        });
+          })
+          .catch((error) => {
+            res.json(`Error populating theme with elements: ${error}`);
+          });
+      });
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.header("Access-Control-Allow-Origin", "*");
+      return res.json({ message: "error occured, please try again." });
+    });
 });
 
-router.put('/:id', (req, res) => {
-    const updateQuery = {}
-    // check color
-    if (req.body.color) {
-        updateQuery.color = req.body.color
-    }
-    // check height
-    if (req.body.height) {
-        updateQuery.height = req.body.height
-    }
-    // check width
-    if (req.body.width) {
-        updateQuery.width = req.body.width
-    }
-    // check textSize
-    if (req.body.textSize) {
-        updateQuery.textSize = req.body.textSize
-    }
+router.put("/:id", (req, res) => {
+  const updateQuery = {};
+  // check color
+  if (req.body.color) {
+    updateQuery.color = req.body.color;
+  }
+  // check height
+  if (req.body.height) {
+    updateQuery.height = req.body.height;
+  }
+  // check width
+  if (req.body.width) {
+    updateQuery.width = req.body.width;
+  }
+  // check textSize
+  if (req.body.textSize) {
+    updateQuery.textSize = req.body.textSize;
+  }
 
-    Theme.findByIdAndUpdate(req.params.id, { $set: updateQuery }, { new: true })
-        .then((theme) => {
-            return res.json({ message: `Theme at ${req.params.id} was updated`, theme: theme });
-        })
-        .catch((error) => {
-            console.log('error inside PUT /themes/:id', error);
-            return res.json({ message: 'error occured, please try again.' });
-        });
+  Theme.findByIdAndUpdate(req.params.id, { $set: updateQuery }, { new: true })
+    .then((theme) => {
+      return res.json({
+        message: `Theme at ${req.params.id} was updated`,
+        theme: theme,
+      });
+    })
+    .catch((error) => {
+      console.log("error inside PUT /themes/:id", error);
+      return res.json({ message: "error occured, please try again." });
+    });
 });
 
 // DELETE route for /themes/:id
-router.delete('/:id', (req, res) => {
-
-    Theme.findByIdAndDelete(req.params.id)
-        .then((result) => {
-            return res.json({ message: `Theme at ${req.params.id} was deleted` });
-        })
-        .catch((error) => {
-            console.log('error inside DELETE /themes/:id', error);
-            return res.json({ message: 'error occured, please try again.' });
-        });
+router.delete("/:id", (req, res) => {
+  Theme.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      return res.json({ message: `Theme at ${req.params.id} was deleted` });
+    })
+    .catch((error) => {
+      console.log("error inside DELETE /themes/:id", error);
+      return res.json({ message: "error occured, please try again." });
+    });
 });
 
 module.exports = router;
--- models/theme.js
+++ models/theme.js
@@ -1,15 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const elementSchema = new mongoose.Schema(
+  {
+    type: String,
+    name: String,
+    xPosition: Number,
+    yPosition: Number,
+    width: String,
+    height: String,
+    backgroundColor: String,
+    fontColor: String,
+  },
+  { timestamps: true }
+);
 
 // create the theme schema
-const themeSchema = new mongoose.Schema({
-  color: String,
-  height: String,
-  width: String,
-  textSize: String,
-}, { timestamps: true });
+const themeSchema = new mongoose.Schema(
+  {
+    name: String,
+    backgroundColor: String,
+    elements: [elementSchema],
+  },
+  { timestamps: true }
+);
 
 // create model
-const Theme = mongoose.model('Theme', themeSchema);
+const Theme = mongoose.model("Theme", themeSchema);
 
 // export the model to be used
 module.exports = Theme;
